698ff88e87b093920c835fa4600215123e50c229
Ahmed Hamza <aeita4@yahoo.com>
2024-07-04 03:04:31 +0300

    setup splash in main activity

--- app/src/main/java/com/hamza/ecommerce/MainActivity.kt
+++ app/src/main/java/com/hamza/ecommerce/MainActivity.kt
@@ -1,20 +1,62 @@
 package com.hamza.ecommerce
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var splashScreen: SplashScreen
     override fun onCreate(savedInstanceState: Bundle?) {
+        initSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        keepSplashScreenFor5Seconds()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun initSplashScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen = installSplashScreen()
+        } else {
+            setTheme(R.style.Theme_Ecommerce)
+        }
+    }
+
+
+    /**
+     * Keep splash screen on-screen indefinitely. This is useful if you're using a custom Activity
+     * for routing.
+     */
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun keepSplashScreenIndefinitely() {
+        splashScreen.setKeepOnScreenCondition { true }
+    }
+
+    /**
+     * Keep splash screen on-screen for longer period. This is useful if you need to load data when
+     * splash screen is appearing.
+     */
+    private fun keepSplashScreenFor5Seconds() {
+        val content = findViewById<View>(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+            override fun onPreDraw(): Boolean {
+                Thread.sleep(3000)
+                content.viewTreeObserver.removeOnPreDrawListener(this)
+                return true
+            }
+        })
     }
 }
