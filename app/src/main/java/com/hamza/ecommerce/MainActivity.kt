644f20ba059c83f0fcb64ccfdea012b2633e1346
Ahmed Hamza <aeita4@yahoo.com>
2024-08-07 17:14:43 +0300

    check if user logged in

--- app/src/main/java/com/hamza/ecommerce/MainActivity.kt
+++ app/src/main/java/com/hamza/ecommerce/MainActivity.kt
@@ -1,30 +1,69 @@
 package com.hamza.ecommerce
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.ViewTreeObserver
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.hamza.ecommerce.data.repository.user.UserPreferencesRepositoryImpl
+import com.hamza.ecommerce.ui.auth.AuthActivity
+import com.hamza.ecommerce.ui.common.viewmodel.UserViewModel
+import com.hamza.ecommerce.ui.common.viewmodel.UserViewModelFactory
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var splashScreen: SplashScreen
+    private val userViewModel: UserViewModel by viewModels {
+        UserViewModelFactory(UserPreferencesRepositoryImpl(this@MainActivity))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         initSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+        checkUserLoggedIn()
         keepSplashScreenFor5Seconds()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun checkUserLoggedIn() {
+        lifecycleScope.launch(Main) {
+            val isLoggedIn = userViewModel.isUserLoggedIn().first()
+            if (isLoggedIn) {
+                setContentView(R.layout.activity_main)
+            } else {
+                gotoAuthActivity()
+            }
+        }
+    }
+
+    private fun gotoAuthActivity() {
+        val intent = Intent(this, AuthActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val option = ActivityOptions.makeCustomAnimation(
+            this,
+            android.R.anim.fade_in,
+            android.R.anim.fade_out
+        )
+        startActivity(intent, option.toBundle())
     }
 
     private fun initSplashScreen() {
--- app/src/main/java/com/hamza/ecommerce/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/hamza/ecommerce/ui/auth/AuthActivity.kt
@@ -18,4 +18,5 @@ class AuthActivity : AppCompatActivity() {
             insets
         }
     }
+
 }
