05e21628e19d9e7d1d4a4f9628bb3e3694ff7387
Ahmed Hamza <aeita4@yahoo.com>
2024-08-13 07:42:19 +0300

    init data binding

--- app/src/main/java/com/hamza/ecommerce/ui/auth/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/hamza/ecommerce/ui/auth/viewmodel/LoginViewModel.kt
@@ -1,12 +1,43 @@
 package com.hamza.ecommerce.ui.auth.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.hamza.ecommerce.data.repository.auth.FirebaseAuthRepository
 import com.hamza.ecommerce.data.repository.user.UserPreferencesRepository
+import com.hamza.ecommerce.data.repository.user.UserPreferencesRepositoryImpl
+import com.hamza.ecommerce.ui.common.viewmodel.UserViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
-class LoginViewModel(private val userPrefs: UserPreferencesRepository) : ViewModel() {
+class LoginViewModel(
+    private val userPrefs: UserPreferencesRepository,
+    private val authRepository: FirebaseAuthRepository
+) : ViewModel() {
+
+    val email = MutableStateFlow("")
+    val password = MutableStateFlow("")
+
+    fun loginWithEmailAndPassword() {
+        viewModelScope.launch {
+            authRepository.loginWithEmailAndPassword(email.value, password.value)
+        }
+    }
+
+}
 
 
-    fun isUserLoggedIn() {
-        //userPrefs.isUserLoggedIn()
+class LoginViewModelFactory(
+    private val userPrefs: UserPreferencesRepository,
+    private val authRepository: FirebaseAuthRepository
+) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST") return LoginViewModel(userPrefs, authRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
+
