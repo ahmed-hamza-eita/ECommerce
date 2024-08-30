d1628c9504b01ec8d0eca8fa31e08c50c2edf44b
Ahmed Hamza <aeita4@yahoo.com>
2024-08-30 21:28:52 +0300

    manage FB auth repository

--- app/src/main/java/com/hamza/ecommerce/data/repository/auth/FirebaseAuthRepository.kt
+++ app/src/main/java/com/hamza/ecommerce/data/repository/auth/FirebaseAuthRepository.kt
@@ -8,4 +8,6 @@ interface FirebaseAuthRepository {
 
     suspend fun loginWithEmailAndPassword(email: String, password: String): Flow<Resource<String>>
     suspend fun loginWithGoogle(idToken: String): Flow<Resource<String>>
+    suspend fun loginWithFacebook(idToken: String): Flow<Resource<String>>
+    fun signOut()
 }
