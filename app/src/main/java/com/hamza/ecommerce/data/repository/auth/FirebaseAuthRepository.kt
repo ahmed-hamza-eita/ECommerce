c3de062e21f0b99b8ec500cd3037b9227b607216
Ahmed Hamza <aeita4@yahoo.com>
2024-09-01 12:43:53 +0300

    enhance auth repository

--- app/src/main/java/com/hamza/ecommerce/data/repository/auth/FirebaseAuthRepository.kt
+++ app/src/main/java/com/hamza/ecommerce/data/repository/auth/FirebaseAuthRepository.kt
@@ -1,13 +1,14 @@
 package com.hamza.ecommerce.data.repository.auth
 
 import com.hamza.ecommerce.data.models.Resource
+import com.hamza.ecommerce.data.models.user.UserDetailsModel
 import kotlinx.coroutines.flow.Flow
 
 interface FirebaseAuthRepository {
 
 
-    suspend fun loginWithEmailAndPassword(email: String, password: String): Flow<Resource<String>>
-    suspend fun loginWithGoogle(idToken: String): Flow<Resource<String>>
-    suspend fun loginWithFacebook(idToken: String): Flow<Resource<String>>
+    suspend fun loginWithEmailAndPassword(email: String, password: String): Flow<Resource<UserDetailsModel>>
+    suspend fun loginWithGoogle(idToken: String): Flow<Resource<UserDetailsModel>>
+    suspend fun loginWithFacebook(idToken: String): Flow<Resource<UserDetailsModel>>
     fun signOut()
 }
