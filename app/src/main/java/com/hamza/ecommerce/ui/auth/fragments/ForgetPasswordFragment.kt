4b53a432c9dfb849967e80b4b23d5421a03fa7ec
Ahmed Hamza <aeita4@yahoo.com>
2024-09-05 16:28:22 +0300

    fix design

--- app/src/main/java/com/hamza/ecommerce/ui/auth/fragments/ForgetPasswordFragment.kt
+++ app/src/main/java/com/hamza/ecommerce/ui/auth/fragments/ForgetPasswordFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.hamza.ecommerce.R
 import com.hamza.ecommerce.databinding.FragmentForgetPasswordBinding
+import com.hamza.ecommerce.ui.auth.viewmodel.ForgetPasswordViewModel
+import com.hamza.ecommerce.ui.auth.viewmodel.ForgetPasswordViewModelFactory
+import com.hamza.ecommerce.ui.auth.viewmodel.LoginViewModel
+import com.hamza.ecommerce.ui.auth.viewmodel.LoginViewModelFactory
 
 
 class ForgetPasswordFragment : BottomSheetDialogFragment() {
@@ -13,6 +19,9 @@ class ForgetPasswordFragment : BottomSheetDialogFragment() {
     private var _binding: FragmentForgetPasswordBinding? = null
     private val binding get() = _binding!!
 
+    private val forgetPasswordViewModel: ForgetPasswordViewModel by viewModels {
+        ForgetPasswordViewModelFactory(requireContext())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,7 +31,7 @@ class ForgetPasswordFragment : BottomSheetDialogFragment() {
         _binding = FragmentForgetPasswordBinding.inflate(inflater, container, false)
         binding.apply {
             lifecycleOwner = viewLifecycleOwner
-            viewmodel = viewmodel
+            viewmodel = forgetPasswordViewModel
 
         }
         return binding.root
@@ -32,6 +41,7 @@ class ForgetPasswordFragment : BottomSheetDialogFragment() {
         super.onDestroyView()
         _binding = null
     }
+
 
     companion object {
         private const val TAG = "ForgetPasswordFragment"
--- app/src/main/java/com/hamza/ecommerce/ui/auth/viewmodel/ForgetPasswordViewModel.kt
+++ app/src/main/java/com/hamza/ecommerce/ui/auth/viewmodel/ForgetPasswordViewModel.kt
@@ -4,18 +4,13 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
-import com.hamza.ecommerce.data.datasource.datastore.AppPreferencesDataSource
 import com.hamza.ecommerce.data.repository.auth.FirebaseAuthRepository
 import com.hamza.ecommerce.data.repository.auth.FirebaseAuthRepositoryImpl
-import com.hamza.ecommerce.data.repository.common.AppDataStoreRepositoryImpl
-import com.hamza.ecommerce.data.repository.common.AppPreferenceRepository
-import com.hamza.ecommerce.data.repository.user.UserPreferenceRepository
-import com.hamza.ecommerce.data.repository.user.UserPreferenceRepositoryImpl
+
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class ForgetPasswordViewModel(
-    private val appPreferencesRepository: AppPreferenceRepository,
-    private val userPreferencesRepository: UserPreferenceRepository,
+
     private val authRepository: FirebaseAuthRepository
 ) : ViewModel() {
 
@@ -28,15 +23,11 @@ class ForgetPasswordViewModel(
 
 class ForgetPasswordViewModelFactory(private val context: Context) :
     ViewModelProvider.Factory {
-    private val appPreferencesRepository =
-        AppDataStoreRepositoryImpl(AppPreferencesDataSource(context))
-    private val userPreferencesRepository = UserPreferenceRepositoryImpl(context)
     private val firebaseAuthRepository = FirebaseAuthRepositoryImpl()
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
         if (modelClass.isAssignableFrom(ForgetPasswordViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST") return ForgetPasswordViewModel(
-                appPreferencesRepository,
-                userPreferencesRepository,
+
                 firebaseAuthRepository
             ) as T
         }
