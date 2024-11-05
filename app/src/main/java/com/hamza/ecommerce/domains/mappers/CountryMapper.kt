cddbb66e638f90c269c648ebbd65edb1bd4ca7ee
aheit <aeita4@yahoo.com>
2024-11-05 18:54:36 +0200

    country mapper class

---
+++ app/src/main/java/com/hamza/ecommerce/domains/mappers/CountryMapper.kt
@@ -0,0 +1,16 @@
+package com.hamza.ecommerce.domains.mappers
+
+import com.hamza.ecommerce.data.models.auth.CountryModel
+import com.hamza.ecommerce.ui.auth.models.CountryUIModel
+
+
+fun CountryModel.toUIModel(): CountryUIModel {
+    return CountryUIModel(
+        id = id,
+        name = name,
+        code = code,
+        currency = currency,
+        image = image,
+        currencySymbol = currencySymbol
+    )
+}
